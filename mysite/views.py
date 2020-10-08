73e0dc03e54ce0f5441d6b02fae8e0d0faf7f632
sumagnadas <sumagnadas@gmail.com>
2020-10-08 10:27:29 +0530

    Updated the view functions.

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone, datetime_safe
 
 
 def index(request):
-    return render(request, 'index.html')
+    timezone.activate(timezone.get_current_timezone())
+    birth_date = datetime_safe.datetime(2006, 1, 11, tzinfo=timezone.get_current_timezone())
+    age = (timezone.now() - birth_date).days // 365
+    return render(request, 'site/index.html', {'age': age})
 
 
 def about(request):
-    return render(request, 'base.html')
+    return render(request, 'site/about.html')
